47c4dc0371219f9a96f4e4d6836d0a4b85d78343
ming <jianmingforpc@gmail.com>
2018-12-04 22:45:06 +0800

    解决close的时候异常输出

--- kafka_logging_handler.py
+++ kafka_logging_handler.py
@@ -21,16 +21,35 @@ class KafkaLoggingHandler(Handler):
         self.partition = partition
         self.producter = KafkaProducer(bootstrap_servers=self.hosts_list, **kargs)
 
+        # 获取 KafkaProducer 的后台线程对象_sender
+        # 通过该线程对象判断 KafkaProducer 是否已经关闭
+        # 问:为什么不通过KafkaProducer对象中的_closed属性判断
+        # 答:因为调用close()方法时，到方法末尾才设置_closed标志，但是连接已经断开了，会导致异常的排除
+        self._pro_sender = getattr(self.producter, "_sender")
+
     def emit(self, record):
+
+        if record.name.startswith('kafka') and not getattr(self._pro_sender, "_running", False):
+            # 如果producter已经关闭，且是kafka模块的日志
+            # 则忽略kafka模块的输出
+
+            # 问:为什么用self._pro_sender中的_closed属性判断
+            # 答: self._pro_sender是 KafkaProducer 的后台线程对象，但是不能用is_alive()方法判断
+            #    因为连接关闭后，还是会有日志输出，而_running属性是首先设置的
+            return
         try:
             msg = self.format(record)
-            self.producter.send(topic=self.topic, value=bytes(msg), partition=self.partition, key=self.key)
-        except Exception as ex:
-            print(ex)
+            self.producter.send(topic=self.topic, value=bytes(msg, encoding="utf8"),
+                                partition=self.partition, key=self.key)
+        except Exception:
             self.handleError(record)
 
     def close(self, timeout=None):
         Handler.close(self)
+        try:
+            self.producter.flush(timeout)
+        except Exception:
+            pass
         self.producter.close(timeout)
 
     def flush(self, timeout=None):
