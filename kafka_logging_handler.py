f6c0e2f99d4751be8c8ff913a87ef28cf0140078
ming <jianmingforpc@gmail.com>
2018-12-04 16:16:16 +0800

    添加注释

--- kafka_logging_handler.py
+++ kafka_logging_handler.py
@@ -1,27 +1,37 @@
 from logging import Handler
 from kafka import KafkaProducer
-# import pykafka
 
 
 class KafkaLoggingHandler(Handler):
 
-    def __init__(self, hosts_list, topic, key=None):
-
+    def __init__(self, hosts_list, topic, key=None, partition=None, **kargs):
+        """
+        Kafka logging handler init function
+        :param hosts_list: ‘host[:port]’ string (or list of ‘host[:port]’ strings)
+        :param topic: kafka topic
+        :param key: the key for kafka productor send msg
+        :param partition: partition for kafka productor send msg
+        :param kargs: Keyword Arguments from KafkaProducer, except bootstrap_servers
+        """
         Handler.__init__(self)
 
         self.hosts_list = hosts_list
         self.topic = topic
         self.key = key
-
-        self.producter = KafkaProducer(bootstrap_servers=self.hosts_list)
+        self.partition = partition
+        self.producter = KafkaProducer(bootstrap_servers=self.hosts_list, **kargs)
 
     def emit(self, record):
         try:
             msg = self.format(record)
-            self.producter.send(topic=self.topic, value=record, key=self.key)
-        except Exception:
+            self.producter.send(topic=self.topic, value=bytes(msg), partition=self.partition, key=self.key)
+        except Exception as ex:
+            print(ex)
             self.handleError(record)
 
-    def close(self):
+    def close(self, timeout=None):
         Handler.close(self)
-        self.producter.close()
+        self.producter.close(timeout)
+
+    def flush(self, timeout=None):
+        self.producter.flush(timeout)
